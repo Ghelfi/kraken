e42092ee81006df7b786437bb13659f085183e2f
Alexandre Ghelfi <alexandre.ghelfi@helsing.ai>
2024-07-01 13:05:25 +0200

    adding hhard check against expected formatting

--- kraken-build/tests/kraken_std/python/tasks/test_ruff_task.py
+++ kraken-build/tests/kraken_std/python/tasks/test_ruff_task.py
@@ -15,11 +15,15 @@ def test__ruff__apply_fmt_success(kraken_project: Project, script: str) -> None:
     ruff()
     kraken_project.context.execute([":fmt"])
 
+    # Check that the formatted file corresponds to the expected one
+    assert kraken_project.directory.joinpath("src/pyfile.py").read_text() == GOOD_SCRIPT
+
 
 def test__ruff__lint_success(kraken_project: Project) -> None:
     (kraken_project.directory / "src").mkdir()
     kraken_project.directory.joinpath("src/pyfile.py").write_text(GOOD_SCRIPT)
     ruff()
+    # Expect linting to run without error on GOOD_SCRIPT
     kraken_project.context.execute([":lint"])
 
 
@@ -27,6 +31,7 @@ def test__ruff__lint_fail(kraken_project: Project) -> None:
     (kraken_project.directory / "src").mkdir()
     kraken_project.directory.joinpath("src/pyfile.py").write_text(BAD_SCRIPT)
     ruff()
+    # Expect linting to fail on BAD_SCRIPT
     with pytest.raises(BuildError, match=r'task ":python\.ruff\.(?:fmt\.check|fmt)" failed'):
         kraken_project.context.execute([":lint"])
 
@@ -37,3 +42,6 @@ def test__ruff__fmt_lint_success(kraken_project: Project, script: str) -> None:
     kraken_project.directory.joinpath("src/pyfile.py").write_text(script)
     ruff()
     kraken_project.context.execute([":fmt", ":lint"])
+
+    # Check that the formatted file corresponds to the expected one
+    assert kraken_project.directory.joinpath("src/pyfile.py").read_text() == GOOD_SCRIPT
